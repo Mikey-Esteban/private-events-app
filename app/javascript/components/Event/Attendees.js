1532199db0f3f7c0362e749dbb74200d4127da95
Mikey Esteban <michaelp.esteban@gmail.com>
2021-02-04 11:02:25 -0500

    Event#show component shows attendees

---
+++ app/javascript/components/Event/Attendees.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Attendees = (props) => {
+  return (
+    <li>{props.attributes.name}</li>
+  )
+}
+
+export default Attendees
--- app/javascript/components/Event/Event.js
+++ app/javascript/components/Event/Event.js
@@ -1,10 +1,11 @@
-import React, { Fragment } from 'react'
+import React, { useState, useEffect, Fragment } from 'react'
 import axios from 'axios'
+import Attendees from './Attendees'
 import Navbar from '../UI/Navbar'
 import Wrapper from '../UI/styles/Wrapper'
 
 const Event = (props) => {
-  console.log(props);
+
   const user = props.location.state.user
   const event = props.location.state.event
   const event_attending = {
@@ -12,14 +13,25 @@ const Event = (props) => {
     attendee_id: user.id
   }
 
-  const handleSubmit = e => {
-    e.preventDefault()
-    console.log(event);
+  const [ attendees, setAttendees ] = useState([])
+
+  useEffect( () => {
+    axios.get(`/api/v1/events/${event.id}`)
+      .then( resp => {
+        setAttendees(resp.data.included)
+      })
+      .catch( resp => console.log(resp) )
+  }, [attendees.length])
+
+  const handleSubmit = () => {
     axios.post(`/api/v1/event_attendings`, {event_attending})
       .then( resp => {})
       .catch( resp => console.log(resp) )
   }
 
+  const attendeesList = attendees.map( item => {
+    return (<Attendees key={item.id} attributes={item.attributes} />)
+  })
 
   return (
     <Fragment>
@@ -31,6 +43,10 @@ const Event = (props) => {
         <div>{event.attributes.date}</div>
         <div>{event.attributes.creator_name}</div>
         <button onClick={handleSubmit}>Attend Event</button>
+      </Wrapper>
+      <Wrapper>
+        <h3>Attendees!</h3>
+        <ul>{attendeesList}</ul>
       </Wrapper>
     </Fragment>
   )
